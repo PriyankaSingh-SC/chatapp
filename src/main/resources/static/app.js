af676937bb5b26e5be29d4f50fd119448ca6be85
Dhrubajyoti Gogoi <37109709+priygithub@users.noreply.github.com>
2019-12-25 01:36:50 +0530

    First draft commit of my chat app

---
+++ src/main/resources/static/app.js
@@ -0,0 +1,54 @@
+var stompClient = null;
+
+function setConnected(connected) {
+    $("#connect").prop("disabled", connected);
+    $("#disconnect").prop("disabled", !connected);
+    if (connected) {
+        $("#conversation").show();
+    }
+    else {
+        $("#conversation").hide();
+    }
+    $("#greetings").html("");
+}
+
+function connect() {
+	alert("in connect method");
+    var socket = new SockJS('/chatapi');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        setConnected(true);
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/topic/greetings', function (greeting) {
+            showGreeting(JSON.parse(greeting.body).content);
+        });
+    });
+}
+
+function disconnect() {
+    if (stompClient !== null) {
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+function sendName() {
+	alert("in sendName method");
+    stompClient.send("/app/chat", {}, JSON.stringify({'name': $("#name").val()}));
+}
+
+function showGreeting(message) {
+	alert("in showGreeting method");
+    $("#greetings").append("<tr><td>" + message + "</td></tr>");
+}
+
+/*
+$(function () {
+    $("form").on('submit', function (e) {
+        e.preventDefault();
+    });
+    $( "#connect" ).click(function() { connect(); });
+    $( "#disconnect" ).click(function() { disconnect(); });
+    $( "#send" ).click(function() { sendName(); });
+});*/
